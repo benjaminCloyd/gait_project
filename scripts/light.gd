bf956b71e226eb9f97a3e8551b9c8382392e27bd
lukedotz <156631341+lukeodtz@users.noreply.github.com>
2025-12-03 15:31:01 -0600

    added var to Light_Large

--- scripts/light.gd
+++ scripts/light.gd
@@ -1,16 +1,23 @@
+@tool
 extends Node2D
 
 @export var light_scale: float = 1.0 : set = set_light_scale
 
-@onready var light: PointLight2D = $PointLight2D
+var light: PointLight2D
 
 
 func _ready() -> void:
-	# Apply the exported value when the scene loads
+	_cache_light()
 	set_light_scale(light_scale)
+
+
+func _cache_light() -> void:
+	if light == null:
+		light = get_node_or_null("PointLight2D")
 
 
 func set_light_scale(value: float) -> void:
 	light_scale = value
-	if is_instance_valid(light):
+	_cache_light()
+	if light:
 		light.texture_scale = light_scale

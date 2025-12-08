ef50c648d3d8617bc6673fee4354f5e87137ab77
Richie Nguyen <rngyen@uiowa.edu>
2025-12-07 20:21:51 -0600

    Fixed Ticks

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -31,25 +31,25 @@ func _on_exit_button_pressed() -> void:
 #Voice Change Slider, right now we have 1 - 5
 func _on_voice_slider_value_changed(value: float) -> void:
 	if(value == 1):
-		print("value is 1")
+		print("V value is 1")
 	elif(value == 2):
-		print("value is 2")
+		print("V value is 2")
 	elif(value == 3):
-		print("value is 3")
+		print("V value is 3")
 	elif(value == 4):
-		print("value is 4")
+		print("V value is 4")
 	elif(value == 5):
-		print("value is 5")
+		print("V value is 5")
 
 #Meaness Change Slider, right now we have 1 - 5
 func _on_mean_slider_value_changed(value: float) -> void:
 	if(value == 1):
-		print("value is 1")
+		print("M value is 1")
 	elif(value == 2):
-		print("value is 2")
+		print("M value is 2")
 	elif(value == 3):
-		print("value is 3")
+		print("M value is 3")
 	elif(value == 4):
-		print("value is 4")
+		print("Mvalue is 4")
 	elif(value == 5):
-		print("value is 5")
+		print("Mvalue is 5")

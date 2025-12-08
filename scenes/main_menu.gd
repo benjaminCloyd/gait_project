dfaaccda530b0789c7a09d97355896480ce4eaab
Richie Nguyen <rngyen@uiowa.edu>
2025-12-07 20:14:13 -0600

    Created basic Options Menu
    Nothing is connected to the sliders yet

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -4,7 +4,8 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$VBoxContainer/StartButton.grab_focus()
-
+	$ColorRect.hide()
+	$VBoxContainer.show()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -17,3 +18,12 @@ func _on_start_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+	
+
+func _on_options_button_pressed() -> void:
+	$VBoxContainer.hide()
+	$ColorRect.show()
+
+func _on_exit_button_pressed() -> void:
+	$ColorRect.hide()
+	$VBoxContainer.show()
